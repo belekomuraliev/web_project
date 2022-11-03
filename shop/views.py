9dc9b51c1c353afde092605ccd86c4457cc74021
belekomuraliev <boke74@mail.ru>
2022-11-04 00:34:18 +0600

    second hw

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import  HttpResponse
+from .models import Item, Purchase
 
 def greetings(request):
     return HttpResponse("Добро пожаловать в наш магазин!")
+
+def list_item(request):
+    items= Item.objects.all()
+    contex = {
+        'items':items,
+    }
+    return render(request, 'list_item.html', contex)
+
+def detail_item(request, item_id):
+
+    item = Item.objects.get(id=item_id)
+    #question = get_object_or_404(Question, id=question_id)
+    contex = {
+        'item':item,
+    }
+    return render(request, 'detail_item.html', contex)
+
 
